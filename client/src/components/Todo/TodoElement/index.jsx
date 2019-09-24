1bc6baa708d7b30a5381299ab92ab21281f901a6
Sebastian Blanco <sebastianblancogonz@gmail.com>
2019-09-24 22:44:47 +0200

    Add checkmark

--- client/src/components/Todo/TodoElement/index.jsx
+++ client/src/components/Todo/TodoElement/index.jsx
@@ -4,19 +4,38 @@ export default class TodoElement extends Component {
   constructor(props) {
     super(props)
   }
+
+  checkMark(completed) {
+    console.log(completed)
+    if (completed) {
+      return (
+        <Fragment>
+          <div className="checkmark">
+            <div className="stem"></div>
+            <div className="kick"></div>
+          </div>
+        </Fragment>
+      )
+    }
+
+    return null
+  }
   render() {
     const todo = this.props
-    console.log(todo)
     return (
       <Fragment>
-        <div className="dot"></div>
-        <div className="todoTitle">
-          <span className="title">
-            <h2>{todo.title}</h2>
-          </span>
+        <div className={'dot ' + (todo && todo.completed ? 'success' : '')}>
+          {this.checkMark(todo.completed)}
         </div>
-        <div className="todoDesc">
-          <p>{todo.description}</p>
+        <div className="todoData">
+          <div className="todoTitle">
+            <span className="title">
+              <h2>{todo.title}</h2>
+            </span>
+          </div>
+          <div className="todoDesc">
+            <p>{todo.description}</p>
+          </div>
         </div>
       </Fragment>
     )
